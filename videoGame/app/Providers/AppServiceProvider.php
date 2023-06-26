13ce011709c8e004ccf457b0174258cdca56bb8e
Boldizsár Balázs <balazsboldizsar2000@gmail.com>
2023-06-26 23:48:28 +0200

    Misc

--- videoGame/app/Providers/AppServiceProvider.php
+++ videoGame/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 use App\Models\Topic;
 use Illuminate\Support\Facades\View;
+use Illuminate\Pagination\Paginator;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -21,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+        Paginator::useBootstrapFive();
         View::composer('*', function ($view) {
             $topics = Topic::all();
             $view->with('topics', $topics);
--- videoGame/resources/views/layouts/app.blade.php
+++ videoGame/resources/views/layouts/app.blade.php
@@ -1,12 +1,4 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
@@ -14,9 +6,10 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    @extends('layouts.main')
+@section('content')
+        <div class="min-h-screen bg-gray-100 font-sans antialiased">
+
 
             <!-- Page Heading -->
             @if (isset($header))
@@ -32,5 +25,4 @@
                 {{ $slot }}
             </main>
         </div>
-    </body>
-</html>
+@endsection
