75453e8143d2d33c4dac068eb87d6f2c0427ae99
Boldizsár Balázs <balazsboldizsar2000@gmail.com>
2023-06-26 23:44:46 +0200

    Web.php routes done

--- videoGame/routes/web.php
+++ videoGame/routes/web.php
@@ -3,6 +3,12 @@
 use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\GameController;
+use App\Http\Controllers\TopicController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,24 +24,50 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/', [Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/home', function () {
+    return view('home');
+})->middleware(['auth', 'verified'])->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
     Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/games/edit/{game}', [GameController::class, 'show'])->name('game.edit');
+    Route::post('/games/{game}/seen', [GameController::class, 'gameisPlayed']) -> name("game.isplayed");
+    Route::get('/games/played', [GameController::class, 'listPlayedGames']) -> name("game.listPlayedGames");
+    Route::delete('/games/played/{play}', [GameController::class, 'deletePlayedGame']) -> name("game.deletePlayedGame");
+
+
+    Route::get('/admin/games/create', [GameController::class, 'addGame'])->name('game.create');
+    Route::post('/admin/games/create', [GameController::class, 'storeGame'])->name('game.create');
+    Route::get('/admin/companies/create', [CompanyController::class, 'create'])->name('company.create');
+    Route::post('/admin/companies', [CompanyController::class, 'store'])->name('companies.store');
+    Route::get('/admin/users', [UserController::class, 'index'])->name('admin.users');
+    Route::delete('/admin/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::get('/admin/games/{game}/edit', [GameController::class, 'edit'])->name('game.edit');
+    Route::patch('/admin/games/{game}', [GameController::class, 'update'])->name('game.update');
+
 
 
 });
 
 //Game
-Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
+Route::get('/games', [GameController::class, 'index'])->name('game.all');
+Route::get('/games/{game}', [GameController::class, 'show'])->name('game.details');
+
+
+
+
+
+//Topic
+Route::get('/topics/{topic}', [TopicController::class, 'show'])->name('game.topic');
 
 
 //Company
+
+Route::get('/companies', [CompanyController::class, 'index'])->name('company.all');
+Route::get('/companies/{company}', [CompanyController::class, 'show'])->name('company.game');
 
 
 require __DIR__.'/auth.php';
